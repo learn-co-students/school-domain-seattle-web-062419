1858af45937995e949b82fca43cb939045e5c0e2
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-30 06:19:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,10 +5,10 @@ class School
 
   def initialize(name)
     @name = name
-    @roster = []
+    @roster = {}
   end
 
   def add_student(grade, student)
-    @roster << student
+    @rosterstudent
   end
 end
